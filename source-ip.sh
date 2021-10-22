82f6b899a8023570681804efab6e182f6a71aa76
Coding Creature <44512062+codingcreature001@users.noreply.github.com>
2021-10-22 16:01:57 -0600

    Update source-ip.sh

--- source-ip.sh
+++ source-ip.sh
@@ -2,11 +2,11 @@ If the X-Forwarded-For is enabled on the load balancer properly and the IP is pu
 
 There is a way to verify if the X-Forwarded-For headers are hitting the API (note: this does not pass through the load balancer). Invoke the API server via curl, adding an X-Forwarded-For header (making sure it is from the bastion host):
 
-```
+~~~
 APISERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 TOKEN=$(kubectl get secret $(kubectl get serviceaccount default -o jsonpath='{.secrets[0].name}') -o jsonpath='{.data.token}' | base64 --decode )
 curl $APISERVER/api/v1/namespaces/default/services \
   --header "Authorization: Bearer $TOKEN" \
   --header "X-Forwarded-For: 7.7.7.7" \
   --insecure
-```
+~~~
